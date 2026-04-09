8778c3c1d47cad121347b9e5782eff660ab185f4
MM58-crypto <mohammed.magdi999@gmail.com>
2026-04-09 13:41:08 +0300

    wooo another commit

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI, Request
 import webhook_handler
-from pyngrok import *
-from pyngrok import ngrok
+import ngrok
+import uvicorn
+#from pyngrok import *
+#from pyngrok import ngrok
+from contextlib import asynccontextmanager
+import uvicorn
 import os
 from dotenv import load_dotenv
+from loguru import logger
 
 load_dotenv()
 app = FastAPI()
 
 ngrok.set_auth_token(os.getenv("NGROK_AUTH_TOKEN"))
-public_url = ngrok.connect(8000)
+APPLICATION_PORT=8000
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Setting up ngrok endpoint")
+    ngrok.set_auth_token(os.getenv("NGROK_AUTH_TOKEN"))
+    tunnel = await ngrok.forward(
+        addr=APPLICATION_PORT,
+    )
+    logger.info(f"Public URL: {tunnel.url()}")
+    yield
+    logger.info("Tearing down ngrok endpoint")
+    ngrok.disconnect()
 
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(webhook_handler.router)
 
 @app.get("/")
@@ -18,4 +36,5 @@ async def root():
     return {"message": "Hello world"}
 
 
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=APPLICATION_PORT, reload=True)
